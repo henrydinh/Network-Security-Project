901e4185c0ffd66ee0c89d37a33e6c6dbd9eb005
Henry Dinh <henryldinh@live.com>
2017-03-21 21:54:19 -0500

    Complete modifyPacket function to modify the RTP packet's sequence number and timestamp.

--- injector.py
+++ injector.py
@@ -2,17 +2,18 @@
 
 from scapy.all import *
 
-# takes in a packet from the main thread and modifies it
-def modifyPacket(packet):
-	None
+# takes in a packet from the main thread and modifies its seq num and timestamp
+def modifyPacket(packet, new_seq, new_ts):
+	packet.sequence = new_seq
+	packet.timestamp = new_ts
 
 # Sends the packet to the address. Timing is left up to the main thread
 def sendPacket(packet, address):
 	None
 
 if __name__ == '__main__':
-	# create a dummy packet and test modifyPacket
-	rtp = RTP(payload_type=17)
+	# create a dummy packet
+	rtp = RTP(payload_type=17, sequence=5, timestamp=17)
 	ip = IP(src="192.168.1.114", dst="192.168.1.25")
 	udp = UDP(sport=1001, dport=1002)
 	test_packet = rtp/udp/ip
@@ -20,6 +21,9 @@ if __name__ == '__main__':
 	print "Original packet:"
 	print test_packet.show()
 	print "\n\n\n"
+	
+	# modify the packet
+	modifyPacket(test_packet, 8, 21)
 	
 	print "Modified packet:"
 	print test_packet.show()
