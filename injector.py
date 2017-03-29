f04573686a863ced2f9351d297a87cf0cce20268
Henry Dinh <henryldinh@live.com>
2017-03-29 10:29:56 -0500

    Create test RTP packet to test the modifyPacketHeader and sendPacket functions.

--- injector.py
+++ injector.py
@@ -3,27 +3,37 @@
 from scapy.all import *
 
 # takes in a packet from the main thread and modifies its seq num and timestamp
-def modifyPacket(packet, new_seq, new_ts):
+def modifyPacketHeader(packet, new_seq, new_ts):
 	packet.sequence = new_seq
 	packet.timestamp = new_ts
 
-# Sends the packet to the address. Timing is left up to the main thread
-def sendPacket(packet, address):
+# replaces a packet's payload	
+def modifyPacketPayload(packet, new_payload):
 	None
+
+# Sends the packet to the address. Timing is left up to the main thread
+def sendPacket(packet):
+	send(packet)
 
 if __name__ == '__main__':
 	# create a dummy packet
-	rtp = RTP(payload_type=17, sequence=5, timestamp=17)
-	ip = IP(src="192.168.1.114", dst="192.168.1.25")
+	rtp = RTP(payload_type=26, sequence=5, timestamp=17)
+	ip = IP(src="192.168.1.101", dst="90.0.0.2")
 	udp = UDP(sport=1001, dport=1002)
-	test_packet = rtp/udp/ip
+	raw = "hello, this is raw data."
+	test_packet = ip/udp/rtp/raw
 	
 	print "Original packet:"
 	print test_packet.show()
 	print "\n\n\n"
 	
 	# modify the packet
-	modifyPacket(test_packet, 8, 21)
+	modifyPacketHeader(test_packet, 8, 21)
 	
-	print "Modified packet:"
+	print "Packet with modified header:"
 	print test_packet.show()
+	print "\n\n\n"
+	
+	print test_packet.getlayer(RTP).load
+	
+	sendPacket(test_packet)
