6316929530686b1cdf68633f1437849ea9bd7614
Travis <travisnwright@gmail.com>
2017-04-05 13:37:13 -0500

    Final version of Listener
    Listener now is a function and accounts for timings

--- listener.py
+++ listener.py
@@ -2,99 +2,108 @@ import sys
 from timeit import default_timer as timer
 from scapy.all import *
 
-printLine = "====================\n"
+def listener():
+        printLine = "===================="
 
-print printLine
-print "Beginning Sniff\n"
-print printLine
+        print printLine
+        print "Beginning Capture Engine"
+        print printLine
 
-start = timer()
+        start = timer()*1000
 
-while True:
-	packetList = sniff(filter="udp and len>1355", count=1)
-	packet = packetList[0]
-	if packet[UDP].len == 1336:
-		break
+        while True:
+                if (timer()*1000) >= start:
+                        while True:
+                                packetList = sniff(filter="udp and len>1355", count=1, timeout=.200)
+                                if not packetList:
+                                        #End the program
+                                        return
+                                packet = packetList[0]
+                                if packet[UDP].len == 1336:
+                                        break
 
-#TODO: Build RTP Layer
-rtpLayer = RTP()/Raw()
-raw = packet[Raw].load
+                        #TODO: Build RTP Layer
+                        rtpLayer = RTP()/Raw()
+                        raw = packet[Raw].load
 
-currByte = ord(raw[0])
+                        currByte = ord(raw[0])
 
-ver = currByte >> 6
-rtpLayer.version = ver
+                        ver = currByte >> 6
+                        rtpLayer.version = ver
 
-pad = currByte >> 5
-pad = pad&1
-rtpLayer.padding = pad
+                        pad = currByte >> 5
+                        pad = pad&1
+                        rtpLayer.padding = pad
 
-ext = currByte >> 4
-ext = ext&1
-rptLayer = ext
+                        ext = currByte >> 4
+                        ext = ext&1
+                        rptLayer = ext
 
-cc = currByte&15
-rtpLayer.numsync = cc
+                        cc = currByte&15
+                        rtpLayer.numsync = cc
 
-currByte = ord(raw[1])
+                        currByte = ord(raw[1])
 
-mark = currByte >> 7
-rtpLayer.marker = mark
+                        mark = currByte >> 7
+                        rtpLayer.marker = mark
 
-payloadType = currByte&127
-rtpLayer.payload = payloadType
+                        payloadType = currByte&127
+                        rtpLayer.payload_type = payloadType
 
-hiByte = ord(raw[2])
-loByte = ord(raw[3])
+                        hiByte = ord(raw[2])
+                        loByte = ord(raw[3])
 
-hiByte = hiByte << 8
-seq = hiByte + loByte
-rtpLayer.sequence = seq
+                        hiByte = hiByte << 8
+                        seq = hiByte + loByte
+                        rtpLayer.sequence = seq
 
-hiByte1 = ord(raw[4])
-hiByte2 = ord(raw[5])
-loByte1 = ord(raw[6])
-loByte2 = ord(raw[7])
+                        hiByte1 = ord(raw[4])
+                        hiByte2 = ord(raw[5])
+                        loByte1 = ord(raw[6])
+                        loByte2 = ord(raw[7])
 
-hiByte1 = hiByte1 << 24
-hiByte2 = hiByte2 << 16
-loByte1 = loByte1 << 8
-time = hiByte1 + hiByte2 + loByte1 + loByte2
-rtpLayer.timestamp = time
+                        hiByte1 = hiByte1 << 24
+                        hiByte2 = hiByte2 << 16
+                        loByte1 = loByte1 << 8
+                        time = hiByte1 + hiByte2 + loByte1 + loByte2
+                        rtpLayer.timestamp = time
 
-hiByte1 = ord(raw[8])
-hiByte2 = ord(raw[9])
-loByte1 = ord(raw[10])
-loByte2 = ord(raw[11])
+                        hiByte1 = ord(raw[8])
+                        hiByte2 = ord(raw[9])
+                        loByte1 = ord(raw[10])
+                        loByte2 = ord(raw[11])
 
-hiByte1 = hiByte1 << 24
-hiByte2 = hiByte2 << 16
-loByte1 = loByte1 << 8
-ssrc = hiByte1 + hiByte2 + loByte1 + loByte2
-rtpLayer.sourcesync = ssrc
+                        hiByte1 = hiByte1 << 24
+                        hiByte2 = hiByte2 << 16
+                        loByte1 = loByte1 << 8
+                        ssrc = hiByte1 + hiByte2 + loByte1 + loByte2
+                        rtpLayer.sourcesync = ssrc
 
-hiByte1 = ord(raw[12])
-hiByte2 = ord(raw[13])
-loByte1 = ord(raw[14])
-loByte2 = ord(raw[15])
+                        hiByte1 = ord(raw[12])
+                        hiByte2 = ord(raw[13])
+                        loByte1 = ord(raw[14])
+                        loByte2 = ord(raw[15])
 
-hiByte1 = hiByte1 << 24
-hiByte2 = hiByte2 << 16
-loByte1 = loByte1 << 8
-sync = hiByte1 + hiByte2 + loByte1 + loByte2
-rtpLayer.sync = sync
+                        hiByte1 = hiByte1 << 24
+                        hiByte2 = hiByte2 << 16
+                        loByte1 = loByte1 << 8
+                        sync = hiByte1 + hiByte2 + loByte1 + loByte2
+                        rtpLayer.sync = sync
 
-#TODO: Rebuild packet with RTP layer
+                        #TODO: Rebuild packet with RTP layer
 
-newRaw = ""
+                        newRaw = ""
 
-for i in range(16, len(raw)):
-        newRaw += raw[i]
+                        for i in range(16, len(raw)):
+                                newRaw += raw[i]
 
-rtpLayer[Raw].load = "".join(newRaw)
+                        rtpLayer[Raw].load = "".join(newRaw)
 
-packet[Raw] = rtpLayer
+                        packet[Raw] = rtpLayer
 
-#TODO: send packet to injector
-print packet.show()
+                        #TODO: send packet to injector
+                        print packet.show()
 
+                        start = (timer()*1000) + 500
+                        
+        return
